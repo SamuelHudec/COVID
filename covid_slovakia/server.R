5571c8bc9ebe3aa0d145d39f8bf920ead10b9aaa
samuel hudec <samuel.hudec@gmail.com>
2020-04-04 16:51:46 +0200

    fit tab is reactive and give option to change model parameters

--- covid_slovakia/server.R
+++ covid_slovakia/server.R
@@ -10,7 +10,7 @@ source("model.R")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-  
+  # grid search controls ####
   # parameters
   b0v = reactive({
     seq(input$b0v[1], input$b0v[2], by = b0v_step)
@@ -22,24 +22,43 @@ shinyServer(function(input, output, session) {
   
   # reacrtive simulationg
   sim_data = reactive({
+    
+    input$update
     # draw the simulated plots with dynamic parametes
-    withProgress({
-      setProgress(message = "Processing ...")
-      sims = corona_explore(b0v(),
-                            input$gammav, 
-                            tmaxv())
+    isolate({
+      withProgress({
+        setProgress(message = "Processing ...")
+        sims = corona_explore(b0v(),
+                              input$gammav, 
+                              tmaxv())
+      })
       sims
     })
   })
   
-  # reactive fitting
-  fit_data = reactive({
+  # one model controls ####
+  # extract best parameters from grid
+  best_params = reactive({
     Vch = sim_data()$Vch[,1,]
     cor = which(1/Vch == max(1/Vch), arr.ind = TRUE)
-    # params
-    b0 = b0v()[cor[1]]
-    tmax = tmaxv()[cor[2]]
-    fit = corona_sim(c(b0, input$gammav, tmax))
+    c(b0v()[cor[1]], input$gammav, tmaxv()[cor[2]])
+  })
+  
+  # update control panel for one fit by best parameters
+  observe({
+    updateNumericInput(session, "b0", value = best_params()[1])
+    updateNumericInput(session, "gamma", value = best_params()[2])
+    updateNumericInput(session, "tmax", value = best_params()[3])
+    })
+  
+  # reactive one model fit
+  fit_data = reactive({
+    # todo: replace by input$update
+    if(input$b0 != 0){
+      fit = corona_sim(c(input$b0, input$gamma, input$tmax))
+    }else{
+      fit = corona_sim(c(best_params()[1], best_params()[2], best_params()[3]))
+    }
     df = data.frame(days = 1:length(fit$Zt), 
                     actual = cumsum(fit$Ct), 
                     fit = cumsum(fit$Zt),
@@ -47,13 +66,12 @@ shinyServer(function(input, output, session) {
     df
   })
   
-  # plotting part
+  # grid tab plots ####
   output$sim_err <- renderPlotly({
     s = sim_data()
     plot_ly(y = tmaxv(), x = b0v(), z = t(1/(s$Vch[,1,])), type = "contour",
             contours = list(showlabels = TRUE)) %>%
-      colorbar(title = "1/Err") %>%
-      layout(title = paste("1/Err, gamma2 =", input$gammav),
+      layout(title = "1/Err",
              xaxis = list(title = "b0v"),
              yaxis = list(title = "tmaxv"))
   })
@@ -63,8 +81,7 @@ shinyServer(function(input, output, session) {
     plot_ly(y = tmaxv(), x = b0v(), z = t(s$Vpp[,1,]), type = "contour",
             contours = list(showlabels = TRUE),
             colorscale = "Greens") %>%
-      colorbar(title = "Percent") %>%
-      layout(title = paste("Percento pozitivnych testov, gamma2 =", input$gammav),
+      layout(title = "Percento pozitivnych testov",
              xaxis = list(title = "b0v"),
              yaxis = list(title = "tmaxv"))
   })
@@ -74,7 +91,7 @@ shinyServer(function(input, output, session) {
     plot_ly(y = tmaxv(), x = b0v(), z = t(s$VNi[,1,]), type = "contour",
             contours = list(showlabels = TRUE),
             colorscale = "Blues") %>%
-      layout(title = paste("Počet infikovaných jedincov, gamma2 =", input$gammav),
+      layout(title = "Počet infikovaných jedincov",
              xaxis = list(title = "b0v"),
              yaxis = list(title = "tmaxv"))
   })
@@ -84,11 +101,12 @@ shinyServer(function(input, output, session) {
     plot_ly(y = tmaxv(), x = b0v(), z = t(s$Vs[,1,]), type = "contour",
             contours = list(showlabels = TRUE),
             colorscale = "Reds") %>%
-      layout(title = paste("Počet umrti, gamma2 =", input$gammav),
+      layout(title = "Počet umrti",
              xaxis = list(title = "b0v"),
              yaxis = list(title = "tmaxv"))
   })
   
+  # fit tab plots ####
   output$best_fit_1 <- renderPlotly({
     ggplotly(
       fit_data() %>% select(days, actual, fit) %>% 
--- covid_slovakia/ui.R
+++ covid_slovakia/ui.R
@@ -13,27 +13,54 @@ shinyUI(fluidPage(
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
-    
     sidebarPanel(
-      sliderInput("b0v", 
-                  label = h4("Slider for b0v"), 
-                  min = 30, 
-                  max = 400, 
-                  value = c(44, 80),
-                  step = b0v_step),
-      numericInput("gammav", 
-                   label = h4("Gammav"), 
-                   value = 1.06,
-                   min = 0.7, 
-                   max = 1.3,
-                   step = gammav_step),
-      sliderInput("tmaxv", 
-                  label = h4("Slider for tmaxv"), 
-                  min = 26, 
-                  max = 45, 
-                  value = c(25, 30),
-                  step = tmaxv_step),
-      submitButton("Apply", icon("refresh")),
+      # panel for grid search
+      conditionalPanel(condition = "input.tabb == 'gridd'",
+        sliderInput(inputId ="b0v", 
+                    label = h4("Slider for b0v"), 
+                    min = 30, 
+                    max = 400, 
+                    value = c(44, 80),
+                    step = b0v_step),
+        numericInput(inputId ="gammav", 
+                     label = h4("Gammav"), 
+                     value = 1.06,
+                     min = 0.7, 
+                     max = 1.3,
+                     step = gammav_step),
+        sliderInput(inputId ="tmaxv", 
+                    label = h4("Slider for tmaxv"), 
+                    min = 26, 
+                    max = 45, 
+                    value = c(25, 30),
+                    step = tmaxv_step),
+        actionButton(inputId = "update",
+                     label = "Apply",
+                     icon = icon("refresh"))
+        ),
+      
+      # panels for one separate fit, default by best params find in grid search
+      conditionalPanel(condition = "input.tabb == 'fitt'",
+        numericInput(inputId ="b0",
+                     label = h4("b0 parameter"),
+                     value = 0,
+                     min = 30,
+                     max = 400,
+                     step = 1),
+        numericInput(inputId ="gamma",
+                     label = h4("Gamma parameter"),
+                     value = 0,
+                     min = 0.7,
+                     max = 1.3,
+                     step = gammav_step),
+        numericInput(inputId ="tmax",
+                     label = h4("Tmax parameter"),
+                     value = 0,
+                     min = 26,
+                     max = 45,
+                     step = 1),
+        p("Po simuláciach vstupy vyššie obsahujú najlepšie nájdené parametre.")
+        ),
       br(),
       hr(),
       p("By Math Knights")
@@ -41,8 +68,8 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      tabsetPanel(id="tabb",
-        tabPanel("Grid", icon = icon("dna"),
+      tabsetPanel(id = "tabb",
+        tabPanel(title = "Grid",
           br(),
           plotlyOutput("sim_err"),
           br(),
@@ -53,14 +80,16 @@ shinyUI(fluidPage(
           plotlyOutput("sim_infi"),
           br(),
           br(),
-          plotlyOutput("sim_deaths")
+          plotlyOutput("sim_deaths"),
+          icon = icon("dna"), value = "gridd"
         ),
-        tabPanel("Fit", icon = icon("chart-line"),
+        tabPanel(title = "Fit",
           br(),
           plotlyOutput("best_fit_1"),
           br(),
           br(),
-          plotlyOutput("best_fit_2")
+          plotlyOutput("best_fit_2"),
+          icon = icon("chart-line"), value = "fitt"
           )
         )
       )
